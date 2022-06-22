4a43e5a5c1c30bdea83df33e7b32dc227db683ee
David <dp8012@pleasantonusd.net>
2022-06-22 01:55:51 -0700

    added CTAs

--- Root/assets/js/custom.js
+++ Root/assets/js/custom.js
@@ -106,7 +106,7 @@ jQuery(function ($) {
             var $anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
+            }, 750, 'easeInOutExpo');
             event.preventDefault();
         });
     });
